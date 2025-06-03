f7529bcd4ae0bf53f5a7c35f0adb9a3cb18932ae
Milan <komurka.milan@email.cz>
2025-06-03 19:17:01 +0200

    Add new feature to control to include an image in the exe file + refactor main frame of the app

--- gui.py
+++ gui.py
@@ -1,5 +1,41 @@
 from generate_password import generate_password
+import tkinter as tk
 from tkinter import *
+import os
+import sys
+
+
+# https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS2
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+bg_color = '#2471A3'
+
+root = Tk()
+root.title('Password generator')
+root.iconbitmap(resource_path('icons\key32x32.ico'))
+root.eval("tk::PlaceWindow . center")
+
+
+frame = tk.Frame(root,
+                width=500,
+                height=300,
+                bg=bg_color
+                )
+
+frame.grid(row=0,
+           column=0,
+           columnspan=5
+           )
+
+frame.pack_propagate(False)
+
+var = tk.StringVar(value='')
 
 
 def entry():
